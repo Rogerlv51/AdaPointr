ba99c3e34ee9231cac765f819cdec35b35b5499c
Rogerlv51 <949608370@qq.com>
2023-07-28 16:04:58 +0800

    update

--- my_data/ch_name.py
+++ my_data/ch_name.py
@@ -1,12 +1,14 @@
 import os
 import glob
 
+# 修改为对应绝对路径
+os.chdir("/home/rogerlv/PoinTr/my_data/train/completion/111")
 completion_list = glob.glob("*f.ply")
 
 for ll in completion_list:
-    os.rename(ll, ll.split(".")[0][:-2]+ll.split(".")[1])
+    os.rename(ll, ll.split(".")[0][:-2]+"."+ll.split(".")[1])
 
-file_list = glob.glob()
+file_list = glob.glob("*")
 
 def save_list_to_txt(lst, filename):
     with open(filename, 'w') as file:
